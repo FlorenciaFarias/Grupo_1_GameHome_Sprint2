376edbe540bfb9dc09e9adfcd3949929ce86735b
Flor <florencia.fariasmm@gmail.com>
2021-11-10 23:21:31 -0300

    Flor, Líder- Views, public, app.js, JSON.

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path= require('path');
+const app = express();
+const port = 3000;
+
+//Ruta elementos estaticos
+app.use(express.static('public'))
+app.listen(port, () => console.log(`Servidor Funcionando! ${port}!`))
+
+//Ruta index
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+
+//Ruta login
+app.get('/login', (req, res) => {
+    res.sendFile('/views/login.html')
+  })
+
+//Ruta product cart
+  app.get('/carritoDeCompras', (req, res) => {
+    res.sendFile(__dirname + '/views/productCart.html')
+  })
+
+//Ruta product detail
+  app.get('/detalleDeProducto', (req, res) => {
+    res.sendFile(__dirname + '/views/productDetail.html')
+  })
+
+//Ruta register
+
+app.get('/registro', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+  })
